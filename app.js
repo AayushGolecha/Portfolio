efb91b53769c485540f38bcd6505a7a3837f7f9b
Aayush <ayushj.aj23@gmail.com>
2023-11-24 17:41:05 +0530

    few changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = 'rgb( 18, 18, 62);';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
